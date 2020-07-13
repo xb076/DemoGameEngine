b52d704f7864696f67fe240e50a02215661e8b26
Maverick <1720502067@qq.com>
2020-07-13 16:43:44 +0800

    Class P12 Completed. Setup events, dispatch window close event.

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -1,21 +1,31 @@
 #include "pch.h"
 #include "Application.h"
 
-#include "Engine/Events/ApplicationEvent.h"
 #include "Engine/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Engine {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		LOG_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -28,6 +38,11 @@ namespace Engine {
 		}
 	}
 
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
 }
 
 
--- Engine/src/Engine/Application.h
+++ Engine/src/Engine/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Engine/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Engine {
@@ -14,7 +16,11 @@ namespace Engine {
 
 		void Run();
 
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
