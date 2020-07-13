d7acfcaf9959867643b1efb72da3c86c0e859c31
Maverick <1720502067@qq.com>
2020-07-13 09:55:46 +0800

    Class P07 completed. Setup premake and update gitignore.

---
+++ premake5.lua
@@ -0,0 +1,118 @@
+workspace "DemoGameEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "DemoGameEngine"
+	location "DemoGameEngine"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}" )
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}" )
+
+
+project "Engine"
+	location "Engine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}" )
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}" )
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ENGINE_PLATFORM_WINDOWS",
+			"ENGINE_BUILD_DLL"
+		}
+		
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "ENGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ENGINE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ENGINE_DIST"
+		optimize "On"
+
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}" )
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}" )
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Engine/vendor/spdlog/include",
+		"Engine/src"
+	}
+
+	links
+	{
+		"Engine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ENGINE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "ENGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ENGINE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ENGINE_DIST"
+		optimize "On"
+
+
